c8e8c6983b657ce006b871da8cabf64bbbe010cd
Diego Araos <daraosn@gmail.com>
2012-05-07 05:16:18 -0400

    working on presence/private channels

--- config.template.js
+++ config.template.js
@@ -1,4 +1,4 @@
 module.exports={
-	'password': 'bGINGS/(ADNfg78GASIMDbkASbj' // replace with your own
-	,'port': 4567 // default: 4567
+	password: 'bGINGS/(ADNfg78GASIMDbkASbj' // replace with your own
+	,port: 4567 // default: 4567
 }
