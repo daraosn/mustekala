b84041f40f38f5502309a781b93acdc094f2ca49
daraosn <daraosn@gmail.com>
2012-05-24 01:21:53 -0400

    allow user to override socket.io config

--- config.js
+++ config.js
@@ -3,7 +3,15 @@ module.exports={
 		password: 'bGINGS/(ADNfg78GASIMDbkASbj'
 		,port: 3001
 	}
-	// add your own config for different environments:
-	,test: { }
-	,production: { }
+	// add your own config for different environments (using NODE_ENV):
+	,test: {}
+	,production: {
+		// example config for heroku:
+		password: 'my_super_secure_password'
+		,port: process.env.PORT
+		,socketIO: {
+			'transports': ["xhr-polling"]
+			,'polling duration': 10
+		}
+	}
 }
